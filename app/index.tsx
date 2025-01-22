0cba20573f1b82270dc744f1ddc6fe100dc608ee
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-23 01:27:08 +0600

    Full setup for tailwind Css

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="h-screen w-screen flex justify-center items-center">
+      <Text className="text-xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
